676fb97a619b1564f3817a2e23401bc4ef7a3e88
Tiago Ferreira <tiferreira12@gmail.com>
2019-10-03 14:51:30 -0300

    fix rubocop issues

--- src/bubble_sort.rb
+++ src/bubble_sort.rb
@@ -3,6 +3,7 @@
 
 def bubble_sort(array)
   array_ = array # method should not be destructive
+
   array_.length.times do |j|
     (array_.length - j).times do |i| # loop till the not ordered element, on first run, last element is already ordered
       yield array, i
@@ -14,9 +15,8 @@ end
 
 def bubble_sort_by(array)
   bubble_sort(array) do |array_, i|
-    if array[i + 1] then
-      order = yield(array[i], array[i+1])
-      # puts order
+    if array[i + 1]
+      order = yield(array[i], array[i + 1])
       array_[i], array_[i + 1] = array_[i + 1], array_[i] if order.positive? # swap elements
     end
   end
@@ -27,7 +27,3 @@ newarr = bubble_sort_by %w[hi hello hey] do |left, right|
 end
 
 puts newarr
-
-# puts bubble_sort [3, 2, 1] # 2, 1, 3
-# puts bubble_sort [3, 4, 2, 1] # 3, 2, 1, 4
-# puts bubble_sort [4, 3, 5, 2, 1] # 3, 2, 1, 4
