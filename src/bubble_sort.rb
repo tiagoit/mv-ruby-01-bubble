8deacc295ad70db2f89c65817186cb340455113e
fedgut <fedgut@gmail.com>
2019-10-06 12:43:51 -0500

    Rewrote to stop accepting block

--- src/bubble_sort.rb
+++ src/bubble_sort.rb
@@ -1,29 +1,17 @@
-#!/usr/bin/env ruby
+# /home/eduardo/.rvm/rubies/ruby-2.6.3/bin/ruby
 # frozen_string_literal: true
 
 def bubble_sort(array)
   array_ = array # method should not be destructive
-
-  array_.length.times do |j|
-    (array_.length - j).times do |i| # loop till the not ordered element, on first run, last element is already ordered
-      yield array, i
+  array_.size.times do |i|
+    (array_.size - i).times do |j|
+      if array_[j + 1] &&
+         array_[j] > array_[j + 1]
+        array_[j], array_[j + 1] = array_[j + 1], array_[j]
+      end
     end
   end
-
   array_
 end
 
-def bubble_sort_by(array)
-  bubble_sort(array) do |array_, i|
-    if array[i + 1]
-      order = yield(array[i], array[i + 1])
-      array_[i], array_[i + 1] = array_[i + 1], array_[i] if order.positive? # swap elements
-    end
-  end
-end
-
-newarr = bubble_sort_by %w[hi hello hey] do |left, right|
-  left.length - right.length
-end
-
-puts newarr
+puts bubble_sort [5, 4, 3, 2, 1, 0]
